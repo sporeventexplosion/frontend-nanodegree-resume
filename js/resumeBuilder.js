62b2bc9d1153d4b9cdc7fef53a45fa8c72c5249f
sporeventexplosion <sporeventexplosion@gmail.com>
2015-06-10 14:52:54 +0300

    feat: Add bio object (info only)

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,20 @@
 "use strict";
+
+var bio = {
+  "name": "Random Guy",
+  "role": "Web Developer",
+  "contacts": {
+    "mobile": "+1 555-555-5555",
+    "email": "random@example.com",
+    "github": "randomperson2574",
+    "twitter": "@random433",
+    "location": "Qeqqata, Greenland",
+  },
+  "welcomeMessage": "*Insert welcome message here*",
+  "skills": [
+    "Skill 1",
+    "Skill 2",
+    "Skill 3"
+  ],
+  "biopic": "images/fry.jpg"
+};

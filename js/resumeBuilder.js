4a30c7fa621c7fd04242d42fa4ea441101289c7e
sporeventexplosion <sporeventexplosion@gmail.com>
2015-06-10 16:05:54 +0300

    Add education object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -18,3 +18,47 @@ var bio = {
   ],
   "biopic": "images/fry.jpg"
 };
+
+
+var education = {
+  "schools": [
+    {
+      "name": "School No. 1",
+      "location": "Kolsky District, Murmansk Oblast, Russia",
+      "degree": "*Insert name of degree here*",
+      "majors": [
+        "Major 1",
+        "Major 2",
+        "Major 3"
+      ],
+      "dates": 1009843200000, // 1/1/2002 00:00:00 UTC in Unix timestamp format (msec)
+      "url": "http://www.example.com/"
+    },
+    {
+      "name": "School No. 2",
+      "location": "Yukon-Koyukuk, AK, United States",
+      "degree": "*Insert name of degree here*",
+      "majors": [
+        "Major 1",
+        "Major 2",
+        "Major 3"
+      ],
+      "dates": 1104537600000,
+      "url": "http://www.example.com/"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title": "Course No. 1",
+      "school": "The Random Online Course Corporation",
+      "date": 1199145600000,
+      "url": "http://www.example.com/"
+    },
+    {
+      "title": "Course No. 2",
+      "school": "The Random Online Course Corporation",
+      "date": 1230768000000,
+      "url": "http://www.example.com/"
+    }
+  ]
+};
